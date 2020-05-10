ec77ee0d0ee0c8813ed31a0dace7f1dd881ac635
super <jamaleldin.ahmed98@gmail.com>
2020-05-10 04:15:32 +0200

    setup project

--- LibrarySystem/src/application/Main.java
+++ LibrarySystem/src/application/Main.java
@@ -1,20 +1,19 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/views/LoginPage.fxml"));
-			Scene scene = new Scene(root);
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
